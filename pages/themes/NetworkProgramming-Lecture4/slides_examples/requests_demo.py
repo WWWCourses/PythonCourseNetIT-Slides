c3dc992c075cd620d8d51f89cd0a8cf1eee729c2
WWWCourses <ivawebcourses@gmail.com>
2022-05-25 09:50:53 +0300

    update

---
+++ pages/themes/NetworkProgramming-Lecture4/slides_examples/requests_demo.py
@@ -0,0 +1,12 @@
+import requests
+
+# requests.get() send the request and returns a Response Object:
+response = requests.get('https://softwareacademy.bg/')
+
+### We can access the request object from the response as:
+print(response.request)
+
+# Let's see the deafult headers sent:
+print(response.request.headers)
+
+
