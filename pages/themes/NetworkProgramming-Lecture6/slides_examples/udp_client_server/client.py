85c3c1f6337e16b545280f32dfc94577846b6477
WWWCourses <ivawebcourses@gmail.com>
2022-11-04 13:10:34 +0200

    update

--- pages/themes/NetworkProgramming-Lecture6/slides_examples/udp_client_server/client.py
+++
@@ -1,7 +0,0 @@
-import socket
-
-# open client socket:
-client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-
-# send data to specific address:
-client.sendto(b'Hello Server',('127.0.0.1', 9999))
