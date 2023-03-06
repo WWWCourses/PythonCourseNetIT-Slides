931fecff34e35322103a780296e3969dfc2af1b5
WWWCourses <ivawebcourses@gmail.com>
2023-03-06 21:48:12 +0200

    update

---
+++ pages/themes/PythonBasics_StringMethods/examples/stringMethod.py
@@ -0,0 +1,14 @@
+# user_name_input = 'ada'
+# user_name = user_name_input.capitalize()
+
+# print(user_name_input)
+# print(user_name)
+
+# example 1
+name = 'Iva'
+print(f'Hello {name}, nice to see you!')
+
+# example 2
+x = 2
+y = 4
+print(f'x+y={x+y}')
