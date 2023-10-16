f81fa9b30a34766640317f20220715b26d185273
WWWCourses <ivawebcourses@gmail.com>
2023-10-16 12:54:20 +0300

    Split NetworkProgramming-Lecture4 into 4&&3

---
+++ pages/themes/NetworkProgramming-Lecture3/slides_examples/udp_client_server/server.py
@@ -0,0 +1,9 @@
+import socket
+
+# open and bind server socket:
+server = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+server.bind(('127.0.0.1', 9999))
+
+# receive data from a client:
+msg,address = server.recvfrom(1024)
+print(f"Received from {address}: {msg.decode('utf-8')}")
