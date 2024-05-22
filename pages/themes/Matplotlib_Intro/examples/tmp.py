60db1d2b0fb280406623366ed41941cd5e95781a
WWWCourses <ivawebcourses@gmail.com>
2024-05-22 18:23:26 +0300

    update

---
+++ pages/themes/Matplotlib_Intro/examples/tmp.py
@@ -0,0 +1,14 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+fig = plt.figure()
+fig.set_facecolor("#B15A5A")
+
+# # add a new axes at the position 1 on a grid of 2 rows and 1 column.
+# fig.add_subplot(211)
+
+# # add a new axes at the position 2 on a grid of 2 rows and 1 column.
+# fig.add_subplot(212)
+
+# # Show the plot
+plt.show()
